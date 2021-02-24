a5f2536f948afd7e6c34be1af24509b2ac11552c
Tom Prince <tom.prince@hocat.ca>
2021-02-23 18:56:08 -0700

    Factor luabundle into a VM and a bunlder using the VM.

--- src/tts/luabundle.py
+++ src/tts/luabundle.py
@@ -1,13 +1,41 @@
-from pathlib import Path
-from typing import Any, Dict, cast
+from __future__ import annotations
 
+from pathlib import Path
+from typing import Any, Dict, TypedDict, cast
+
+import attr
 from node_vm2 import NodeVM, VMServer
 
 
-class Bundler:
-    def __init__(self, module_dir: Path) -> None:
-        self.module_dir = module_dir
-        self.modules: Dict[str, Any] = {}
+class _BundleVM:
+    # https://github.com/Benjamin-Dobell/luabundle#unbundled-data
+    Metadata = TypedDict(
+        "Metadata",
+        {
+            # "identifiers" -- omitted
+            "luaVersion": str,
+            "rootModuleName": str,
+            "version": str,
+        },
+    )
+    Module = TypedDict(
+        "Module",
+        {
+            "name": str,
+            "content": str,
+            # "start": FilePosition,
+            # "end": FilePosition,
+        },
+    )
+    UnbundledData = TypedDict(
+        "UnbundledData",
+        {
+            "metadata": Metadata,
+            "modules": Dict[str, Module],
+        },
+    )
+
+    def __init__(self) -> None:
         self._server: VMServer = VMServer()
         self._server.start()
         self._vm: NodeVM = NodeVM(self._server)
@@ -15,21 +43,61 @@ class Bundler:
         self._module = self._vm.run(
             Path(__file__).with_name("luabundle.js").read_text()
         )
-        if self.module_dir.exists():
-            for module in self.module_dir.iterdir():
-                if module.is_file() and module.suffix == ".lua":
-                    self._module.call_member(
-                        "writeFile", module.name, module.read_text(encoding="utf-8")
-                    )
 
     def __del__(self) -> None:
         self._vm.destroy()
         self._server.close()
 
+    def write_file(self, name: str, script: str) -> None:
+        self._module.call_member("writeFile", name, script)
+
+    def unbundle_string(self, script: str) -> UnbundledData:
+        return cast(
+            _BundleVM.UnbundledData, self._module.call_member("unbundleString", script)
+        )
+
+    def bundle(self, script: str) -> str:
+        return cast(
+            str,
+            self._module.call_member(
+                "bundleString",
+                script,
+                {
+                    "luaVersion": "5.2",
+                    "isolate": True,
+                    "paths": ["?.lua"],
+                    "metadata": True,
+                },
+            ),
+        )
+
+
+@attr.s(auto_attribs=True)
+class Bundler:
+    module_dir: Path
+    _vm: _BundleVM = attr.ib(init=False, default=attr.Factory(_BundleVM))
+
+    def __attrs_post_init__(self) -> None:
+        if self.module_dir.exists():
+            for module in self.module_dir.iterdir():
+                if module.is_file() and module.suffix == ".lua":
+                    self._vm.write_file(module.name, module.read_text(encoding="utf-8"))
+
+    def bundle(self, lua_script: str) -> str:
+        return self._vm.bundle(lua_script)
+
+
+@attr.s(auto_attribs=True)
+class Unbundler:
+    module_dir: Path
+    modules: Dict[str, Any] = attr.ib(init=False, default=attr.Factory(dict))
+    _vm: _BundleVM = attr.ib(init=False, default=attr.Factory(_BundleVM))
+
     def unbundle(self, lua_script: str) -> str:
         if not lua_script.startswith("-- Bundled by luabundle "):
             return lua_script
-        unbundled = self._module.call_member("unbundleString", lua_script)
+
+        unbundled = self._vm.unbundle_string(lua_script)
         root_module = unbundled["modules"].pop(unbundled["metadata"]["rootModuleName"])
         for name, module in unbundled["modules"].items():
             if name in self.modules:
@@ -40,19 +108,4 @@ class Bundler:
                 self.module_dir.joinpath(f"{name}.lua").write_text(
                     module["content"], encoding="utf-8"
                 )
-        return cast(str, root_module["content"])
-
-    def bundle(self, lua_script: str) -> str:
-        return cast(
-            str,
-            self._module.call_member(
-                "bundleString",
-                lua_script,
-                {
-                    "luaVersion": "5.2",
-                    "isolate": True,
-                    "paths": ["?.lua"],
-                    "metadata": True,
-                },
-            ),
-        )
+        return root_module["content"]
--- src/tts/unpack.py
+++ src/tts/unpack.py
@@ -3,15 +3,14 @@ from pathlib import Path
 from typing import Any, Dict, List
 
 from .config import Config
-from .luabundle import Bundler
+from .luabundle import Unbundler
 from .utils.formats import format_json, to_unix
 
 
 def _unpack_objects(
     objects: List[Dict[str, Any]],
     base_path: Path,
-    config: Config,
-    bundler: Bundler,
+    unbundler: Unbundler,
 ) -> None:
     index = []
     for obj in objects:
@@ -30,16 +29,14 @@ def _unpack_objects(
         script_path = path.joinpath("script.lua")
         if obj_script.strip():
             script_path.write_text(
-                bundler.unbundle(to_unix(obj_script)), encoding="utf-8"
+                unbundler.unbundle(to_unix(obj_script)), encoding="utf-8"
             )
         elif script_path.exists():
             script_path.unlink()
 
         contained_objects = obj.pop("ContainedObjects", None)
         if contained_objects is not None:
-            _unpack_objects(
-                contained_objects, path.joinpath("contained"), config, bundler
-            )
+            _unpack_objects(contained_objects, path.joinpath("contained"), unbundler)
 
         script_state = obj.pop("LuaScriptState", "null")
         script_state_path = path.joinpath("script-state.json")
@@ -64,10 +61,10 @@ def _unpack_objects(
 
 
 def unpack(*, savegame: Dict[str, Any], config: Config) -> None:
-    bundler = Bundler(config.lua_modules)
+    unbundler = Unbundler(config.lua_modules)
 
     global_script = to_unix(savegame.pop("LuaScript"))
-    config.global_script.write_text(bundler.unbundle(global_script), encoding="utf-8")
+    config.global_script.write_text(unbundler.unbundle(global_script), encoding="utf-8")
 
     script_state = savegame.pop("LuaScriptState")
     if script_state:
@@ -85,6 +82,6 @@ def unpack(*, savegame: Dict[str, Any], config: Config) -> None:
     else:
         config.xml_ui.unlink()
 
-    _unpack_objects(savegame.pop("ObjectStates"), config.objects, config, bundler)
+    _unpack_objects(savegame.pop("ObjectStates"), config.objects, unbundler)
 
     config.savegame.write_text(format_json(savegame), encoding="utf-8")
